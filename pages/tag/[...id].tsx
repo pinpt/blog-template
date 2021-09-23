1aa9b53a38cf31bdc363af38880f86225fcc653e
Chris Bowley <cbowley@pinpoint.com>
2021-09-23 12:09:38 +0100

    Fix tag entries static path generation; bump @pinpt/react

--- pages/tag/[...id].tsx
+++ pages/tag/[...id].tsx
@@ -1,5 +1,4 @@
-import NextHead from 'next/head';
-import { fetchAnalytics, fetchContentPaginated, fetchSiteWithContentCount, titleCase } from '@pinpt/react';
+import { fetchAnalytics, fetchContentPaginated, fetchSite, fetchSiteWithContentCount, titleCase } from '@pinpt/react';
 import config from '../../pinpoint.config';
 import EntriesPage, { IEntriesPageProps } from '../../components/EntriesPage';
 
@@ -7,8 +6,47 @@ const Page = (props: IEntriesPageProps) => <EntriesPage {...props} />;
 
 export default Page;
 
-export async function getServerSideProps(context: any) {
-	const params = context.params;
+const getPathsForTag = async (tag: string) => {
+	const { count } = await fetchSiteWithContentCount(config, tag);
+	const pages = Math.ceil(count / config.pageSize);
+	const paths = [];
+
+	let next = 0;
+
+	for (let i = 1; i <= pages; i++) {
+		const res = await fetchContentPaginated(config, {
+			tag,
+			offset: next,
+			limit: config.pageSize,
+			after: true,
+			projection: ['id', 'dateAt'],
+		});
+		paths.push({
+			params: {
+				id: [tag, String(i), String(next), String(pages)],
+			},
+		});
+		next = res.after?.dateAt ?? 0;
+	}
+
+	return paths;
+}
+
+export async function getStaticPaths() {
+	const site = await fetchSite(config);
+	const tags = site?.theme?.homepage?.tags ?? [];
+
+	const tagPaths = await Promise.all(
+		tags.map((tag) => getPathsForTag(tag))	
+	);
+
+	return {
+		paths: tagPaths.flatMap((paths) => paths),
+		fallback: 'blocking', // server render on-demand if page doesn't exist
+	};
+}
+
+export async function getStaticProps({ params }: { params: { id: [string, string, string, string] } }) {
 	const tag = params.id[0];
 	const pageNumber = parseInt(params.id[1]);
 	const offset = parseInt(params.id[2] ?? '0');
@@ -43,5 +81,6 @@ export async function getServerSideProps(context: any) {
 			pageCount,
 			analytics,
 		},
+		revalidate: 1,
 	};
 }
